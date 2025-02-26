86265674579b70a7e0e779b6d60a2fc49697cf6f
alaa1010mamdouh <alaamamdouhalsayd1010@gmail.com>
2025-02-26 16:41:18 +0200

    startDev

--- Talabat.APIs/WeatherForecast.cs
+++ Talabat.APIs/WeatherForecast.cs
@@ -4,6 +4,7 @@ namespace Talabat.APIs
     {
         public DateTime Date { get; set; }
 
+        public int id { get; set; }
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
